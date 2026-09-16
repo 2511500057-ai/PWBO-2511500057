3d454140e85415c489dfaf4401cbcdf72ed05ed2
Steven Marcelino <2511500057@mahasiswa.atmaluhur.ac.id>
2026-09-16 14:24:51 +0700

    Menambahkan file baru

--- app/core/App.php
+++ app/core/App.php
@@ -1,17 +1,22 @@
 <?php
 
 class App {
+    protected $controller = 'Home'; //controller default
+    protected $method = 'index'; //method default 
+    protected $params = []; //parameter default
+
     public function __construct()
     {
         $url = $this->parseURL();
         var_dump($_GET);
+
     }
 
     public function parseURL()
     {
         if(isset($_GET['url'])){
             $url = rtrim($_GET['url'], '/');
-            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = filter_var($url, FILTER_SANITIZE_URL); //membersihkan url dari karakter aneh
             $url = explode('/', $url);
             return $url;
         }
